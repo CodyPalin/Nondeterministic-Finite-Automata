f96eed7585089d80ac59e9591e27faba3e096c19
CodyPalin <43323493+CodyPalin@users.noreply.github.com>
2019-04-08 23:06:45 -0600

    revert

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,236 +1,75 @@
 package fa.nfa;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import fa.State;
-import fa.dfa.DFA;
 
+import fa.State;
 
 /**
- * Implementation of an NFA
+ * Implementation of an NFA state.
+ * 
  * @author Cody Palin, Dominick Edmonds
  */
-public class NFA implements NFAInterface{
-	private Set<NFAState> states;
-	private NFAState startState;
-	private Set<Character> alphabet;
+public class NFAState extends State {
+	private Boolean isFinal; //To remember it's type, ala DFAState
+	private HashMap<Character,List<NFAState>> delta; //set of transitions for this state
 	
-	public NFA() {
-		states = new LinkedHashSet<NFAState>();
-		alphabet = new LinkedHashSet<Character>();
-		alphabet.add('e');
+	public NFAState(String name){
+		initialize(name);
+		this.isFinal = false;
 	}
 	
-	@Override
-	public void addStartState(String name) {
-		NFAState s = checkIfExists(name);
-		if(s == null){
-			s = new NFAState(name);
-			addState(s);
-		} else {
-			if(s.isFinal()){}
-			else System.out.println("WARNING: A state who's name = \"" + name + "\" already exists in the NFA and is not final");
+	public void addTransition(char onSymb, NFAState to){
+		if(delta.containsKey(onSymb)) { //has a list already
+			//get that list
+			List<NFAState> tempList = delta.get(onSymb);
+			tempList.add(to);
+			delta.remove(onSymb);
+			delta.put(onSymb, tempList);
+		} else { //no list exists for this symbol
+			List<NFAState> newList = new ArrayList<NFAState>(); //TODO had to choose an implementation of List. Correct choice?
+			newList.add(to);
+			delta.put(onSymb, newList);
 		}
-		startState = s;
-	}
 
-	@Override
-	public void addState(String name) {
-		NFAState s = checkIfExists(name);
-		if( s == null){
-			s = new NFAState(name);
-			addState(s);
-		} else {
-			System.out.println("WARNING: A state who's name = \"" + name + "\" already exists in the NFA");
-		}
 	}
 	
-	/**
-	 * Adds a NFAState to the list of states
-	 * @param s NFA State to add
-	 */
-	private void addState(NFAState s){
-		states.add(s);
+	public NFAState(String name, Boolean isFinal) {
+		initialize(name);
+		this.isFinal = isFinal;
 	}
-
-	@Override
-	public void addFinalState(String name) {
-		NFAState s = checkIfExists(name);
-		if( s == null ){
-			s = new NFAState(name, true);
-			addState(s);
-		} else {
-			System.out.println("WARNING: A state with name \"" + name + "\" already exists in the NFA");
-		}
+	
+	private void initialize(String name){
+		this.name = name;
+		delta = new HashMap<Character, List<NFAState>>();
 	}
 	
 	/**
-	 * Checks if the given state exits in this NFA. Very similar to the 
-	 * DFA.java implementation.
-	 * @param name The name of the state we're looking for
-	 * @return null if the state does not exist, returns the state otherwise
+	 * Getter for state finality
+	 * @return True if state is final
 	 */
-	private NFAState checkIfExists(String name) {
-		NFAState rv = null;
-		for(NFAState s : states) {
-			if(s.getName().equals(name)) { //State w/ this name already exists
-				rv = s;
-				break;
-			} 
-		}
-	
-		return rv;
-	}
-
-	@Override
-	public void addTransition(String fromState, char onSymb, String toState) {
-		NFAState from = checkIfExists(fromState);
-		NFAState to = checkIfExists(toState);
-		if(from == null){
-			System.err.println("ERROR: No NFA state exists with name " + fromState);
-			System.exit(2);
-		} else if (to == null){
-			System.err.println("ERROR: No NFA state exists with name " + toState);
-			System.exit(2);
-		}
-		from.addTransition(onSymb, to);
-		
-		if(!alphabet.contains(onSymb)){
-			alphabet.add(onSymb);
-		}
-	}
-
-	@Override
-	public Set<NFAState> getStates() {
-		return states;
-	}
-
-	@Override
-	public Set<NFAState> getFinalStates() {
-		Set<NFAState> ret = new LinkedHashSet<NFAState>();
-		for(NFAState s : states){
-			if(s.isFinal()){
-				ret.add(s);
-			}
-		}
-		return ret;
-	}
-
-	@Override
-	public State getStartState() {
-		return startState;
-	}
-
-	@Override
-	public Set<Character> getABC() {
-		return alphabet;
+	public boolean isFinal(){
+		return isFinal;
 	}
 	
-	//queue for getDFA
-	private Queue<Set<NFAState>> dfaStates = new LinkedList<>();
-	
-	private void findDFAStates(Set<NFAState> s){
-		for(char a : alphabet){ //for every alphabet symbol
-			Set<NFAState> closureSet = new LinkedHashSet<NFAState>();
-			for(NFAState state : s){ //get the union of the closure of each element in the set
-				Set<NFAState> toState = getToState(state, a); //could be multiple states a single alphabet symbol goes to
-				for(NFAState t : toState)
-				{
-					closureSet.addAll(eClosure(t));
+	/**
+	 * Retrieves the set of states that <code>this</code> transitions to
+	 * on the given symbol
+	 * @param symb - the alphabet symbol
+	 * @return the list of all states the symbol leads to
+	 */
+	public Set<NFAState> getTo(Character symb){ 
+		Set<NFAState> retval = new HashSet<NFAState>();
+		retval.add(this);
+		List<NFAState> list = delta.get(symb);
+			if(list != null) { 
+				retval = new HashSet<NFAState>(); //I}TODO: Determine if HashSet is best option here
+				for(NFAState s : list) { 
+					retval.add(s);
 				}
 			}
-			if(!dfaStates.contains(closureSet)) //if its a new set for the queue, need to make another row for this set as a state.
-			{
-				dfaStates.add(closureSet);
-				findDFAStates(closureSet);
-				// TODO: might have to add transitions here somehow?
-			}
-		}
+		return retval;
 	}
-	@Override
-	public DFA getDFA() { 
-		//TODO: THEOREM 1.39
-		DFA retVal = new DFA();
-		//BFS on the NFA for this; loop over a queue (where the queue elements are sets of NFAStates. 
-		//set up start state as first element in queue
-		//find DFAStartState = eClosure(getStartState)
-		Set<NFAState> DFAStartState = eClosure(startState);
-		dfaStates.add(DFAStartState);
-		retVal.addStartState(DFAStartState.toString());
-		//find DFAStates ((all subsets of states from NFA = state list for DFA))
-		findDFAStates(DFAStartState); //queue is now set up
-		
-		//find DFAFinalStates = any member of DFA's new set of state subset's from the NFA that contain a state that was final in the NFA
-		Set<Set<NFAState>> DFAFinalStates = new LinkedHashSet<Set<NFAState>>();
-		Queue<Set<NFAState>> q = dfaStates;
-		for(Set<NFAState> set: q)
-		{
-			boolean isFinal = false;
-			for(NFAState state : set)
-			{
-				if (state.isFinal()){
-					DFAFinalStates.add(set);
-					retVal.addFinalState(set.toString());
-					isFinal = true;
-					break;
-				}
-			}
-			if(!isFinal)
-				retVal.addState(set.toString());
-		}
-		
-		retVal.addState("T");//add trap state
-		String trapState = "T";
-		//TODO: transition function ((!)) 
-		//take list of DFA states from earlier; for each:
-				//check each alphabet symbol for a transition
-				//if no transition for a symbol, go to trap state T
-				//else, verify which state it will go to in the DFAStates list
-		for(Set<NFAState> set: q)
-		{
-			for(char abc : alphabet)
-			{
-				String nextStateString = trapState;
-				Set<NFAState> nextState = new LinkedHashSet<NFAState>();
-				for(NFAState state : set)
-				{
-					for(NFAState toState : getToState(state, abc))
-					{
-						nextState.add(toState);
-					}
-				}
-				if(!nextState.isEmpty())
-				{
-					nextStateString = nextState.toString();
-				}
-				retVal.addTransition(set.toString(), abc, nextStateString);
-			}
-		}
-			
-		
-		
-		return retVal;
-	}
-
-	@Override
-	public Set<NFAState> getToState(NFAState from, char onSymb) {
-		return from.getTo(onSymb);
-	}
-	private Set<NFAState> closureStates = new LinkedHashSet<NFAState>();
-	@Override
-	public Set<NFAState> eClosure(NFAState s) {
-		//Set<NFAState> states = new LinkedHashSet<NFAState>();
-		Set<NFAState> newStates = s.getTo('e');
-		if(closureStates.addAll(newStates)) //if set changed
-			for(NFAState t : newStates){
-				Set<NFAState> lowerstates = eClosure(t);
-				if(lowerstates != null)
-					closureStates.addAll(lowerstates);
-			}
-		else
-			return null;
-		return closureStates;
-	}
-
 }
