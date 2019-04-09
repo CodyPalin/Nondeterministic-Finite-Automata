ec265d6e0c96d5d6302cac4981c700b1f0c2f1d1
CodyPalin <43323493+CodyPalin@users.noreply.github.com>
2019-04-09 02:32:12 -0600

    fixed a ton of stuff
    getTo needs to return null in some cases, as symbols that are not e will not necessarily return themself.

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -16,30 +16,11 @@ public class NFAState extends State {
 	private Boolean isFinal; //To remember it's type, ala DFAState
 	private HashMap<Character,List<NFAState>> delta; //set of transitions for this state
 	
-	/**
-	 * Default constructor
-	 * @param name the state name
-	 */
 	public NFAState(String name){
 		initialize(name);
 		this.isFinal = false;
 	}
 	
-	/**
-	 * Overlaoded constructor that sets the state type
-	 * @param name the state name
-	 * @param isFinal the type of state: true - final, false - non-final.
-	 */
-	public NFAState(String name, Boolean isFinal) {
-		initialize(name);
-		this.isFinal = isFinal;
-	}
-	
-	/**
-	 * Add the transition from <code> this </code> object
-	 * @param onSymb the alphabet symbol
-	 * @param toState to DFA state
-	 */
 	public void addTransition(char onSymb, NFAState to){
 		if(delta.containsKey(onSymb)) { //has a list already
 			//get that list
@@ -55,10 +36,11 @@ public class NFAState extends State {
 
 	}
 	
-	/**
-	 * Helper function for object creation
-	 * @param name Name of the NFA
-	 */
+	public NFAState(String name, Boolean isFinal) {
+		initialize(name);
+		this.isFinal = isFinal;
+	}
+	
 	private void initialize(String name){
 		this.name = name;
 		delta = new HashMap<Character, List<NFAState>>();
@@ -79,8 +61,7 @@ public class NFAState extends State {
 	 * @return the list of all states the symbol leads to
 	 */
 	public Set<NFAState> getTo(Character symb){ 
-		Set<NFAState> retval = new HashSet<NFAState>();
-		retval.add(this);
+		Set<NFAState> retval = null;
 		List<NFAState> list = delta.get(symb);
 			if(list != null) { 
 				retval = new HashSet<NFAState>(); //I}TODO: Determine if HashSet is best option here
